5b303685ac05bec764a59608ef3f737579d442e4
Owner <Owner@DESKTOP-O6UGATP>
2026-02-03 14:46:02 +0900

    課題4完了

---
+++ Hello_World_yamanehayato/src/basicClass/Animal.java
@@ -0,0 +1,22 @@
+package basicClass;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog obj = new Dog();
+		String animalName = obj.name;
+		System.out.println(animalName);
+		
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		int dogNumber = obj.number;
+		System.out.println(dogNumber);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		System.out.println(now.format(formatter));
+	}
+}
---
+++ Hello_World_yamanehayato/src/basicClass/Dog.java
@@ -0,0 +1,18 @@
+package basicClass;
+
+public class Dog {
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String name = "Dog";
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int number = 3;
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	Dog(){
+		this.name = "犬";
+	}
+
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	Dog(int number) {
+		this.number = number;
+		}
+}
